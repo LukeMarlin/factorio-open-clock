34b5446ae13b35876cbdae7eaa132620340e6cc7
Luke Marlin <luke.marlin@viacesi.fr>
2020-05-16 12:20:15 +0200

    Initial release

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "string-setting",
+        name = "open-clock-ui-visible",
+        setting_type = "runtime-per-user",
+        default_value = "yes",
+        allowed_values = {"yes", "no"},
+        order = "1"
+    }
+})
